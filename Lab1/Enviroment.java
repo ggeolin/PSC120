c3baac32b778a2a3bed1e158d120b6dce5d9c51f
ggeolin <51928314+ggeolin@users.noreply.github.com>
2021-01-12 17:17:41 -0800

    Add files via upload

---
+++ Lab1/Enviroment.java
@@ -0,0 +1,13 @@
+package month_list;
+
+public class Enviroment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Agent new_agent = new Agent();
+		
+		// new_agent.<methodName>(args)
+		
+	}
+
+}
