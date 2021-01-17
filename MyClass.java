1bfccbcc16e24a55a32e6d897c9894ef6cc14cb9
Frankzh0901 <49966932+Frankzh0901@users.noreply.github.com>
2021-01-17 10:46:47 +0800

    Add files via upload
    hw1

---
+++ MyClass.java
@@ -0,0 +1,55 @@
+package MyMath;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		try {
+			MyClass myclass = new MyClass();
+			myclass.arithmetic("add",20,25);
+		}
+		catch(Exception e) {
+			System.out.println("Enter valid argument plz!");
+		}
+		
+
+	}
+	public double arithmetic(String operator, double value1, double value2) {
+		double result;
+		String key = operator.toLowerCase();
+		//System.out.println(key);
+		switch(key){
+		 default:
+		    System.out.println("Operator "+"'"+operator+"'"+" is not defined");
+		    break;
+	    case "add" :
+	    	result = value1+ value2;
+	    	System.out.println(result);
+	    	break;
+	    case "plus":
+	    	result = value1+ value2;
+	    	System.out.println(result);
+	    	break;
+	    case "subtract":
+	    	result = value1 - value2;
+	    	System.out.println(result);
+	    	break;
+	    case "minus":
+	    	result = value1 - value2;
+	    	System.out.println(result);
+	    	break;
+	    case "multiply":
+	    	result = value1 * value2;
+	    	System.out.println(result);
+	    	break;
+	    case "times":
+	    	result = value1 * value2;
+	    	System.out.println(result);
+	    	break;
+	    case "divide":
+	    	result = value1 / value2;
+	    	System.out.println(result);
+		}
+		return 0;
+		
+}
+}
