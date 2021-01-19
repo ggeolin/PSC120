ef3d152c7636040d68169e869a3f841f62cd0275
tld10 <77367127+tld10@users.noreply.github.com>
2021-01-18 21:14:02 -0800

    Add files via upload

--- HW1/MyMath.java
+++ HW1/MyMath.java
@@ -1,53 +1,55 @@
 package talkers;
 
 public class MyMath {
-
-    public static double arithmetic(String operator, double value1, double value2) {
-    	String key = operator.toLowerCase();
-    	double val = 0;
-
+	
+	public static double arithmetic(String operator, double value1, double value2) {
+		//subtracts, add, multiply, divide
+		double result = 0;
+		String key = operator.toLowerCase();
+		
 		switch(key) {
 			case "add":
-				val = value1 + value2;
+				result = value1 + value2;
 				break;
 			case "plus":
-				val = value1 + value2;
+				result = value1 + value2;
 				break;
 			case "+":
-				val = value1 + value2;
+				result = value1 + value2;
 				break;
 			case "subtract":
-				val = value1 - value2;
+				result = value1 - value2;
 				break;
 			case "minus":
-				val = value1 - value2;
+				result = value1 - value2;
 				break;
 			case "-":
-				val = value1 - value2;
+				result = value1 - value2;
 				break;
 			case "multiply":
-				val = value1 * value2;
+				result = value1 * value2;
 				break;
 			case "times":
-				val = value1 * value2;
+				result = value1 * value2;
 				break;
 			case "*":
-				val = value1 * value2;
+				result = value1 * value2;
 				break;
 			case "divide":
-				val = value1 / value2;
+				result = value1 / value2;
 				break;
 			case "/":
-				val = value1 / value2;
+				result = value1 / value2;
 				break;
 			default:
-			    System.out.println("Operator "+"'"+operator+"'"+" is not defined");
-		}
-		return val;
-	 }
-
+				System.out.println("Operator "+"'"+operator+"'"+" is not defined");
+				result = Double.NaN; //return not a number if operator not defined
+		}//end switch statement
+		return result;
+	}
+	
 	public static void main(String[] args) {
-		double x = arithmetic("divide", 2, 1);
+		double x = arithmetic("*",2,3);
 		System.out.println(x);
 	}
 
