6cde99b3ac7c02517ecba1db4d910ce1aeffb09b
ggeolin <51928314+ggeolin@users.noreply.github.com>
2021-01-27 13:47:50 -0800

    Update GUIfreezing.java

--- Lab2/GUIfreezing.java
+++ Lab2/GUIfreezing.java
@@ -11,7 +11,6 @@ public class GUIfreezing extends GUIStateSweep {
 	public GUIfreezing(SimStateSweep state, int gridWidth, int gridHeight, Color backdrop, Color agentDefaultColor,
 			boolean agentPortrayal) {
 		super(state, gridWidth, gridHeight, backdrop, agentDefaultColor, agentPortrayal);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public static void main(String[] args) {
